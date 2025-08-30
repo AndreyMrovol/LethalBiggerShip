1dfaea5802208bf1c823fb8d22a59dd16200c1ee
s1ckboy <sickxedits@gmail.com>
2025-08-30 19:04:17 +0200

    basic config setup

---
+++ BiggerShip/LocalConfigManager.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx.Configuration;
+using BiggerShip.Enums;
+using MrovLib;
+
+namespace BiggerShip;
+internal class LocalConfigManager : MrovLib.ConfigManager
+{
+    internal static ConfigEntry<LoggingType> Debug { get; private set; }
+
+    internal static ConfigEntry<ChargeStationPlacement> ChargeStation { get; private set; }
+    internal static ConfigEntry<MagnetLeverPlacement> MagnetLever { get; private set; }
+    private LocalConfigManager(ConfigFile config)
+        : base(config)
+    {
+        Debug = configFile.Bind("General", "Logging levels", LoggingType.Basic, "Enable debug logging");
+        ChargeStation = configFile.Bind("Placements", "Charge Station", ChargeStationPlacement.Right, "Change the charge station's placement");
+        MagnetLever = configFile.Bind("Placements", "Magnet Lever", MagnetLeverPlacement.Front, "Change the magnet lever's placement");
+    }
+
+    internal static new void Init(ConfigFile config)
+    {
+        Instance = new LocalConfigManager(config);
+    }
+}
