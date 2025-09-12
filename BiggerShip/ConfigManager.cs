c01385b5987cffbc003faa4b5fa16bcff7c65661
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-09-12 22:29:32 +0200

    configmanager is freed

--- BiggerShip/ConfigManager.cs
+++ BiggerShip/ConfigManager.cs
@@ -4,22 +4,26 @@ using MrovLib;
 
 namespace BiggerShip;
 
-internal class ConfigManager : MrovLib.ConfigManager
+internal class ConfigManager
 {
 	internal static ConfigEntry<LoggingType> Debug { get; private set; }
 	internal static ConfigEntry<ChargeStationPlacement> ChargeStation { get; private set; }
 	internal static ConfigEntry<MagnetLeverPlacement> MagnetLever { get; private set; }
 
+	public static ConfigManager Instance { get; private set; }
+	public static ConfigFile configFile;
+
 	private ConfigManager(ConfigFile config)
-		: base(config)
 	{
+		configFile = config;
 		Debug = configFile.Bind("General", "Logging levels", LoggingType.Basic, "Enable debug logging");
+
 		ChargeStation = configFile.Bind("Placement", "Charge Station", ChargeStationPlacement.Right, "Change the charge station's placement");
-		MagnetLever = configFile.Bind("Placement", "Magnet Lever", MagnetLeverPlacement.Front, "Change the magnet lever's placement");
+		MagnetLever = configFile.Bind("Placement", "Magnet Lever", MagnetLeverPlacement.Back, "Change the magnet lever's placement");
 	}
 
-	internal static new void Init(ConfigFile config)
+	internal static void Init(ConfigFile config)
 	{
-		Instance = new LocalConfigManager(config);
+		Instance = new ConfigManager(config);
 	}
 }
