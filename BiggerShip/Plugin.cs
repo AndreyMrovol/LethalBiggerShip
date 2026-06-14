83bf561cbfbe536014122b7c9d614add174a80e4
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-06-14 23:00:16 +0200

    Update Plugin.cs

--- BiggerShip/Plugin.cs
+++ BiggerShip/Plugin.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace BiggerShip
 {
-	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 	[BepInIncompatibility("mborsh.WiderShipMod")]
 	[BepInIncompatibility("MelanieMelicious.2StoryShip")]
 	[BepInDependency("MrovLib", BepInDependency.DependencyFlags.HardDependency)]
@@ -20,7 +20,7 @@ namespace BiggerShip
 		internal static ManualLogSource logger;
 		internal static Logger debugLogger = new("Debug", MrovLib.LoggingType.Developer);
 
-		internal static Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+		internal static Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
 
 		internal static TooManySuitsCompat TooManySuitsCompat;
 		internal static ScienceBirdTweaksCompat ScienceBirdTweaksCompat;
@@ -56,7 +56,7 @@ namespace BiggerShip
 			UniversalRadarCompat.Init();
 
 			// Plugin startup logic
-			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 		}
 	}
 }
